b5ce23ab67f50c27d559327a02cea7de5b0e3d82
Eeli Koivisto <Eeli.Koivisto@metropolia.fi>
2026-09-13 20:18:39 +0300

    Projektin kolmas kohta

--- peliprojekti/joku.py
+++ peliprojekti/joku.py
@@ -1,5 +1,19 @@
 import random
 
+# Pelinimen vaihto funktio
+def nimen_vaihto():
+    uusi_nimi = input('Uusi nimi: ')
+    print(f'Pelinimesi on vaihdettu! Uusi pelinimesi on {uusi_nimi}.')
+    return uusi_nimi
+
+# Nopan heitto funktio
+def nopan_heitto():
+    noppa = random.randint(1, 6)
+    print(f'NOPPA: Heitit luvun {noppa}!')
+
+def nayta_status(nimi):
+    print(f'STATUS: Pelaaja: {nimi}')
+
 nimi = input('Mikä on nimesi: ')
 ikä = int(input('Mikä on ikäsi: '))
 
@@ -12,9 +26,10 @@ else:
     #Päävalikko
     while True:
         print('\n--- Päävalikko ---')
-        print("status  - Näytä pelaajan tiedot")
-        print("lopeta  - Sulje peli")
-        print("noppa   - Heitä noppaa")
+        print('status  - Näytä pelaajan tiedot')
+        print('lopeta  - Sulje peli')
+        print('noppa   - Heitä noppaa')
+        print('uusi nimi   - Vaihda pelinimesi')
 
         komento = input('\nAnna komento: ')
 
@@ -22,10 +37,15 @@ else:
         if komento == 'lopeta':
             print(f'Kiitos pelaamisesta {nimi}!')
             break
+
         elif komento == 'status':
-            print(f'STATUS: Pelaaja: {nimi}')
+            nayta_status()
+
         elif komento == 'noppa':
-            noppa = random.randint(1, 6)
-            print(f'NOPPA: Heitit luvun {noppa}!')
+            nopan_heitto()
+
+        elif komento == 'uusi nimi':
+            nimen_vaihto()
+
         else:
             print('Tuntematon komento. Yritä uudelleen.')
